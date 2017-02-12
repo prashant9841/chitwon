633a45783f4fc33ef4c496c740fbba633a5a4607
zoro <itachi9841@gmail.com>
2017-02-12 13:18:49 +0545

    profile designs

--- resources/assets/js/components/backend/menuBar.vue
+++ resources/assets/js/components/backend/menuBar.vue
@@ -16,15 +16,13 @@
 			  return {
 			    menus: [
 			      { name: 'Dashboard',link: '/dashboard', index: '1'},
-			      { name: 'Users',link: '/dashboard/users', index: '1'},
-			      { name: 'Pages', link: '/dashboard/pages', index: '1.1'},
-			      { name: 'Post', link: '/dashboard/posts', index: '1.2'},
-			      { name: 'Media', link: '/dashboard/gallery', index: '2'},
-			      { name: 'Events', link: '/dashboard/events', index: '2.1'},
-			      { name: 'taso', link: 'Paso', index: '3'},
-			      { name: 'Laso', link: 'Paso', index: '3.1'},
-			      { name: 'Paso', link: 'Paso', index: '3.1.1'},
-			      { name: 'taso', link: 'Paso', index: '4'}
+			      { name: 'Users',link: '/dashboard/users', index: '2'},
+			      { name: 'Pages', link: '/dashboard/pages', index: '3'},
+			      { name: 'Post', link: '/dashboard/posts', index: '4'},
+			      { name: 'Media', link: '/dashboard/gallery', index: '5'},
+			      { name: 'Events', link: '/dashboard/events', index: '6'},
+			      { name: 'Profile', link: '/dashboard/profile', index: '7'}
+
 			    ]
 			  }
 			},
---
+++ resources/assets/js/components/backend/profile.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="user-profile">
+		<el-row :gutter="20">
+			<el-col :span="6">
+				<el-card class="box-card">				  	
+				  	<div class="userdiv">
+				  		<img :src="user.image" alt="">
+				  		<h3>{{user.name}}</h3>
+				  	</div>
+				  	<div class="navs">
+				  		<ul>
+				  			<li><a href="/dashboard/profile">Profile</a></li>
+				  			<li><a href="/dashboard/profile/edit">Edit Profile</a></li>
+				  			<li><a href="/dashboard/profile/settings">Settings</a></li>
+				  		</ul>
+				  	</div>
+				</el-card>
+			</el-col>
+		    <el-col :span="18">
+		  		<el-card class="box-card">
+				  	<div class="card-head">
+				  		<h4>Your profile information</h4>
+				  	</div>
+				  	<el-row :gutter="10">
+				  		<el-col :span="12">
+				  			<p>Full Name: {{user.name}}</p>
+				  		</el-col>	
+				  		<el-col :span="12">
+				  			<p>Email: {{user.email}}</p>				  			
+				  		</el-col>	
+				  	</el-row>
+				</el-card>
+		    </el-col>
+		</el-row>
+	</div>	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				user:{
+					name: 'User Name',
+					image: 'asdasdasdas',
+					email: 'info@domain.com',
+					phone: '98XXXXXXXXX'
+				}
+			}
+		}
+	}
+</script>
